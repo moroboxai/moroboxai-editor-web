e5335924031d7ef6544df86566f396514f19d3b8
Nauja <jeremymorosi@hotmail.fr>
2022-06-23 20:47:52 +0200

    fix compilation to cjs and es

--- gulpfile.js
+++ gulpfile.js
@@ -38,15 +38,9 @@ const webpackConfig = (lib, output, options, library, prod) => ({
 
 gulp.task('cjs', () => {	
 	return gulp.src('./src/index.ts')
-        .pipe(ts.createProject('tsconfig.json')())
-        .pipe(gulp.dest('lib/cjs'));
-});
-
-gulp.task('es', () => {	
-	return gulp.src('./src/index.ts')
         .pipe(gulpWebpack(webpackConfig(
             'lib/es',
-            'index.js',
+            'index.cjs',
             {
                 experiments: {
                     outputModule: true
@@ -54,11 +48,17 @@ gulp.task('es', () => {
             },
             {
                 library: {
-                    type: "module"
+                    type: "commonjs"
                 }
             },
             true
         ), webpack))
+        .pipe(gulp.dest('lib/cjs'));
+});
+
+gulp.task('es', () => {	
+	return gulp.src('./src/**/*.ts')
+        .pipe(ts.createProject('tsconfig.json')())
         .pipe(gulp.dest('lib/es'));
 });
 
